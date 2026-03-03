5bdd62b9c92f4925879da06ca11fc3e9cb7b17a0
erickfialho8-tech <erickfialho8@gmail.com>
2026-03-03 13:57:31 -0300

    Add files via upload

---
+++ Ex003.py
@@ -0,0 +1,7 @@
+nome=input('Qual é o seu nome?')
+print('Olá {}!'.format(nome))
+
+s=input('Como você está?')
+if s=='Estou bem':
+    print('Fico feliz que esteja bem!')
+else: print('Espero que melhore logo! Como posso ajudar?')
